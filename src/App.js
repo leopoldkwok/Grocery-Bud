59b5fcc5492181c2065cacba38b7cc29f1885728
leopoldkwok <leopoldkwok@gmail.com>
2021-02-28 16:34:27 +0000

    Edit Item

--- src/App.js
+++ src/App.js
@@ -20,6 +20,19 @@ function App() {
       showAlert(true, 'danger', 'please enter value');
     } else if (name && isEditing) {
       // deal with edit
+      setList(
+        list.map((item) => {
+          console.log('item', item);
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
     } else {
       // show alert
       showAlert(true, 'success', 'item added to the list');
@@ -43,6 +56,13 @@ function App() {
     setList(list.filter((item) => item.id !== id));
   };
 
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -63,7 +83,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
